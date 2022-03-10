ef4fe32bb5a02d28e6ffa1542afdf0f5e7c65b8e
Daniel Conceição <daniel.colares@gmail.com>
2022-03-09 23:07:03 -0300

    🔨 Add Register element

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 
 import React from 'react';
-import { Dashboard } from './src/screens/Dashboard';
+// import { Dashboard } from './src/screens/Dashboard';
+import { Register } from './src/screens/Register';
 import { ThemeProvider } from 'styled-components'
 
 
@@ -27,7 +28,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      <Register />
     </ThemeProvider>
   )
 }
